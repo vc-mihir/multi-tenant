af578f887aa8ccfcef5db4c987bdc64250777543
vc-mihir <mihir.kothari@viitor.cloud>
2026-04-15 11:02:04 +0530

    fix: Change the name of the RegisterUserController to CompanyRegistrationController

--- routes/auth.php
+++ routes/auth.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\Auth\EmailVerificationPromptController;
 use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
-use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\CompanyRegistrationController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,10 +16,10 @@ Route::middleware('guest')->group(function () {
     Route::get('admin/login', [AdminAuthController::class, 'create'])
         ->name('admin.login');
 
-    Route::get('register', [RegisteredUserController::class, 'create'])
+    Route::get('register', [CompanyRegistrationController::class, 'create'])
         ->name('register');
 
-    Route::post('register', [RegisteredUserController::class, 'store']);
+    Route::post('register', [CompanyRegistrationController::class, 'store']);
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
