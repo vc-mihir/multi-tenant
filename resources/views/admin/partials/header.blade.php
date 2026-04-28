5b6437613ab7894892f01a8a8ec09bede7969f76
vc-mihir <mihir.kothari@viitor.cloud>
2026-04-28 11:08:13 +0530

    Remove the notification from the header

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -25,14 +25,6 @@
     </div>
 
     <div class="flex items-center space-x-4">
-        <button
-            class="p-2.5 text-slate-400 hover:text-teal-600 hover:bg-teal-50 rounded-xl transition-all duration-200 relative">
-            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-            </svg>
-            <span class="absolute top-2 right-2.5 block w-2 h-2 rounded-full bg-red-500 border-2 border-white"></span>
-        </button>
 
         <div class="h-8 w-px bg-slate-200 mx-2"></div>
 
