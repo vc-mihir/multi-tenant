fe83da4b3917440ca8a43833429ae11cbf50813d
vc-mihir <mihir.kothari@viitor.cloud>
2026-05-13 11:52:29 +0530

    Implement tenant migration seeding system with master TenantDatabaseSeeder

--- database/seeders/Tenant/TenantDatabaseSeeder.php
+++ database/seeders/Tenant/TenantDatabaseSeeder.php
@@ -2,13 +2,7 @@
 
 namespace Database\Seeders\Tenant;
 
-use App\Models\Central\Company as CentralCompany;
-use App\Models\Central\CompanyDatabase;
-use App\Models\Tenant\Company as TenantCompany;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Spatie\Permission\Models\Permission;
-use Spatie\Permission\Models\Role;
 
 class TenantDatabaseSeeder extends Seeder
 {
@@ -17,36 +11,8 @@ class TenantDatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $dbName = DB::connection('tenant')->getDatabaseName();
-
-        $companyDatabase = CompanyDatabase::where('db_name', $dbName)->first();
-
-        if (!$companyDatabase) {
-            return;
-        }
-
-        $centralCompany = $companyDatabase->company;
-
-        if (!$centralCompany) {
-            return;
-        }
-
-        TenantCompany::updateOrCreate(
-            ['master_company_id' => $centralCompany->id],
-            [
-                'company_name'      => $centralCompany->company_name,
-                'subdomain'         => $centralCompany->subdomain,
-                'company_email'     => $centralCompany->company_email,
-                'website'           => $centralCompany->website,
-                'license_number'    => $centralCompany->license_number,
-                'address'           => $centralCompany->address,
-                'country'           => $centralCompany->country,
-                'state'             => $centralCompany->state,
-                'city'              => $centralCompany->city,
-                'password'          => $centralCompany->password,
-                'status'            => $centralCompany->status,
-                'email_verified_at' => $centralCompany->email_verified_at,
-            ]
-        );
+        $this->call([
+            TenantCompanySeeder::class,
+        ]);
     }
 }
