41925bc04cd8cf5e4b3396dd3c455b42f3b9b678
vc-mihir <mihir.kothari@viitor.cloud>
2026-05-26 18:07:29 +0530

    feat: replace fonts.bunny.net CDN with self-hosted @fontsource npm packages

--- resources/views/central/admin/login.blade.php
+++ resources/views/central/admin/login.blade.php
@@ -6,9 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Super Admin Login</title>
 
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700" rel="stylesheet" />
-
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,9 +8,6 @@
 
     <title>@yield('title', 'Admin Dashboard') | MultiTenant</title>
 
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700" rel="stylesheet" />
-
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 
